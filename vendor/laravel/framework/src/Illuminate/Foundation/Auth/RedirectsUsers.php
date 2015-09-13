bf9b4341e8e087a41423129bc435959c448c2280
Jovvy Bersamin <jovvybersamin99@gmail.com>
2015-09-13 20:46:42 +0800

    added node_modules and vendor

---
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RedirectsUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Support\Facades\Auth;
+
+trait RedirectsUsers
+{
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        if (property_exists($this, 'redirectPath')) {
+            return $this->redirectPath;
+        }
+
+        if (Auth::user()->isAdmin())
+        {
+            return property_exists($this, 'redirectToIfAdmin') ? $this->redirectToIfAdmin : '/home';
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
+    }
+}
